04f0f8d2042923295a416c65738c0905e7f6377c
Pfumiko <50150564+OlegSolomatin@users.noreply.github.com>
2022-11-22 23:05:48 +0300

    add login page and validation form

---
+++ pages/login.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Layout from "../components/Layout";
+import Link from "next/link";
+import { useForm } from "react-hook-form"
+
+export default function Login() {
+
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm()
+
+    const submitHandler = ({email, password}) => {
+        console.log(email,password)
+    }
+
+    return (
+        <Layout title={'Login'}>
+            <form action="" className={'mx-auto max-w-screen-md'} onSubmit={handleSubmit(submitHandler)}>
+                <h1 className={'mb-4 text-lg'}>Login</h1>
+                <div className={'mb-4'}>
+                    <label htmlFor="email">
+                        Email
+                    </label>
+                    <input type="email"
+                           className="w-full"
+                           id="email"
+                           autoFocus
+                           {...register('email', {
+                               required: 'Please enter your email',
+                               pattern: {
+                                   value: /^[a-zA-Z0-9_,+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-,]+$/i,
+                                   message: 'Please enter valid email'
+                               }
+                           })}
+                    />
+                    {errors.email && (
+                        <div className={'text-red-500'}>{errors.email.message}</div>
+                    )}
+                </div>
+                <div className={'mb-4'}>
+                    <label htmlFor="password">
+                        Password
+                    </label>
+                    <input type="password"
+                           className="w-full"
+                           id="password"
+                           autoFocus
+                           {...register('password', {
+                               required: 'Please enter your password',
+                               minLength: { value: 6, message: 'Please enter at least 6 characters'}
+                           })}
+                    />
+                    {errors.password && (
+                        <div className={'text-red-500'}>{errors.password.message}</div>
+                    )}
+                </div>
+                <div className={'mb-4'}>
+                    <button className={'prymaryButton'}>
+                        Log In
+                    </button>
+                </div>
+                <div className="mb-4">Don&apos;t have an account? &nbsp;
+                    <Link href={'/register'}>Register</Link>
+                </div>
+            </form>
+        </Layout>
+    )
+}
